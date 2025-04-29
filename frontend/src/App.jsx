92024f277bc1ee40c7baa8d03af5ce94ca42694b
Royal <royaldsouza2004@gmail.com>
2025-04-29 19:09:44 +0530

    loading added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,27 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import Layout from "./components/Layout";
 import Loading from "./components/Loading";
-import Search from "./pages/Search";
-import ENewspaper from "./pages/archive/ENewspaper";
-import EMagazine from "./pages/archive/EMagazine";
-import Profile from "./pages/Profile";
-import Login from "./pages/Login";
 import axios from "./api/axios";
-import PDFViewingPage from "./pages/PDFViewingPage";
+
+const Layout = React.lazy(() => import("./components/Layout"));
+const Search = React.lazy(() => import("./pages/Search"));
+const ENewspaper = React.lazy(() => import("./pages/archive/ENewspaper"));
+const EMagazine = React.lazy(() => import("./pages/archive/EMagazine"));
+const Profile = React.lazy(() => import("./pages/Profile"));
+const Login = React.lazy(() => import("./pages/Login"));
+const PDFViewingPage = React.lazy(() => import("./pages/PDFViewingPage"));
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [loading, setLoading] = useState(true);
-  const [userName, setUserName] = useState("")
+  const [userName, setUserName] = useState("");
 
   const checkAuth = async () => {
     try {
       const res = await axios.get("/auth/check");
       setIsLoggedIn(res.data.authenticated);
-      setUserName(res.data.userName)
+      setUserName(res.data.userName);
     } catch (err) {
       setIsLoggedIn(false);
     } finally {
@@ -36,27 +37,29 @@ const App = () => {
 
   return (
     <>
-      <Toaster position="bottom-center" reverseOrder={false} /> 
-      <Routes>
-        {isLoggedIn ? (
-        <Route path="/" element={<Layout userName={userName} setIsLoggedIn={setIsLoggedIn} />}>
-            <Route index element={<Navigate to="/search" />} />
-            <Route path="search" element={<Search />} />
-            <Route path="view-pdf" element={<PDFViewingPage />} />
-            <Route path="archive">
-              <Route path="e-newspaper" element={<ENewspaper />} />
-              <Route path="e-magazine" element={<EMagazine />} />
+      <Toaster position="bottom-center" reverseOrder={false} />
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          {isLoggedIn ? (
+            <Route path="/" element={<Layout userName={userName} setIsLoggedIn={setIsLoggedIn} />}>
+              <Route index element={<Navigate to="/search" />} />
+              <Route path="search" element={<Search />} />
+              <Route path="view-pdf" element={<PDFViewingPage />} />
+              <Route path="archive">
+                <Route path="e-newspaper" element={<ENewspaper />} />
+                <Route path="e-magazine" element={<EMagazine />} />
+              </Route>
+              <Route path="profile" element={<Profile />} />
+              <Route path="*" element={<Navigate to="/search" />} />
             </Route>
-            <Route path="profile" element={<Profile />} />
-            <Route path="*" element={<Navigate to="/search" />} />
-          </Route>
-        ) : (
-          <>
-            <Route path="/login" element={<Login onLoginSuccess={() => setIsLoggedIn(true)} isLoggedIn={isLoggedIn} setUserName={setUserName} />} />
-            <Route path="*" element={<Navigate to="/login" />} />
-          </>
-        )}
-      </Routes>
+          ) : (
+            <>
+              <Route path="/login" element={<Login onLoginSuccess={() => setIsLoggedIn(true)} isLoggedIn={isLoggedIn} setUserName={setUserName} />} />
+              <Route path="*" element={<Navigate to="/login" />} />
+            </>
+          )}
+        </Routes>
+      </Suspense>
     </>
   );
 };
--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,9 +1,7 @@
 const Loading = () => {
       return (
-        <div className="h-screen flex items-center justify-center">
-          <p className="text-2xl animate-pulse">Loading...</p>
-        </div>
-      );
+          <section className='w-full h-[100vh] flex justify-center items-center'><div className="h-40 w-40 rounded-[50%]  border-t-[1rem] border-black border-l-[1rem] animate-spin border-r-[1rem] border-b-[1rem] border-r-white border-b-white"></div></section>
+        );
     };
     
     export default Loading;
